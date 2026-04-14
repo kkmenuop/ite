08ac5b5820a3d8e315f40e83452cf1043d943fbf
Davi Melo <davi19melo@gmail.com>
2026-04-14 20:19:07 -0300

    add new script link

--- src/lib/links.ts
+++ src/lib/links.ts
@@ -1766,4 +1766,22 @@ export const links: Links = {
       },
     ],
   },
+  yyy: {
+    imageUrl: 'thumbb78.png',
+    title: 'Survive the Apocalypse SCRIPT',
+    createdAt: '2026-04-14T23:52:49.706Z',
+    redirectUrl: 'https://link-target.net/330762/GrTHqHlcY312',
+    actions: [
+      {
+        id: 1,
+        type: 'like_comment',
+        url: 'https://youtu.be/VdvnKjWD51Y',
+      },
+      {
+        id: 2,
+        type: 'subscribe',
+        url: 'https://www.youtube.com/@KAKAZiT',
+      },
+    ],
+  },
 };
