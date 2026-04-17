b7038805584747c5f462932f379041a77080f0ff
Davi Melo <davi19melo@gmail.com>
2026-04-17 16:49:24 -0300

    add new script link

--- src/lib/links.ts
+++ src/lib/links.ts
@@ -1838,4 +1838,22 @@ export const links: Links = {
       },
     ],
   },
+  ac: {
+    imageUrl: 'thumbb82.png',
+    title: 'Murder Mystery 2 SCRIPT',
+    createdAt: '2026-04-17T23:52:49.706Z',
+    redirectUrl: 'https://link-hub.net/330762/eDJg1H0BO7Mq',
+    actions: [
+      {
+        id: 1,
+        type: 'like_comment',
+        url: 'https://youtu.be/EHdiRz8PRXU',
+      },
+      {
+        id: 2,
+        type: 'subscribe',
+        url: 'https://www.youtube.com/@KAKAZiT',
+      },
+    ],
+  },
 };
